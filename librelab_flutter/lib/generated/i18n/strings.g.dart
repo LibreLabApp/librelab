6a5dfe411d86414c3e2fa16d4c1f29acddc26c1b
Ellet <echo.ellet@gmail.com>
2026-07-09 00:33:21 +0300

    chore(deps): add cupertino_icons for web build, and bump dart_style

--- librelab_flutter/lib/generated/i18n/strings.g.dart
+++ librelab_flutter/lib/generated/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 238 (119 per locale)
 ///
-/// Built on 2026-07-08 at 16:38 UTC
+/// Built on 2026-07-08 at 21:30 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
